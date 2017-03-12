1d752b850bb353f7aaf3f9469948ea3b1b00c931
Tyler Matrix <tylermatrix0@gmail.com>
2017-03-11 22:31:11 -0600

    Install simple_form and created article show pages

--- db/migrate/20170312040642_create_articles.rb
+++ db/migrate/20170312040642_create_articles.rb
@@ -2,7 +2,7 @@ class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
       t.string :title
-      t.text :context
+      t.text :content
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20170312040642) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
-    t.text     "context"
+    t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
